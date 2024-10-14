1bf039b5af6cb71b47215269fa3ee3f3ff8da161
Mariaazzi <mariatheresaelkazzi@gmail.com>
2024-10-14 19:49:21 +0300

    Done with the form design

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,27 @@
+import { Inria_Serif, Inter } from '@next/font/google';
+import Form from '../component/Form';
+
+const Inriaserif = Inria_Serif({
+  subsets: ['latin'],
+  weight: ['400', '700']
+});
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '700']
+});
+
+
 export default function Page() {
   
     return(
-      <div>Contact</div>
+      <div className="p-8">
+        <div className="text-3xl font-bold pb-4">Get in Touch</div>
+        <div className={Inriaserif.className}>We're here to help your brand forward with tailored strategies and measurable results. Transform your digital presence NOW. 
+        Whether you have a question, need a consultation, or just want to learn more about our services, feel free to reach out. Our team at The Shift is ready to assist you.</div>
+        {/* form */}
+        <Form />
+      </div>
     );
 
 }
